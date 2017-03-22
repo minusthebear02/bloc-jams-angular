34d0f3b9aa893b518703c8091f64fb09ead7df69
Mark Froehlich <mfroeh2@gmail.com>
2017-03-22 00:12:50 -0400

    Angular Checkpoint 5: Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 (function() {
     function config($stateProvider, $locationProvider) {
         $locationProvider
-            .html5Model({
+            .html5Mode({
             enabled: true,
             requireBase: false
         });
@@ -9,6 +9,7 @@
         $stateProvider
             .state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/bloc-jams-angular/app/templates/landing.html'
             })
         
@@ -19,6 +20,7 @@
         
             .state('collection', {
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl:'/bloc-jams-angular/app/templates/collection.html'
             })
     }
---
+++ app/scripts/fixtures.js
@@ -0,0 +1,46 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {title: 'Blue', duration: 161.71, audioUrl: 'assets/music/bloc_jams_music/blue'},
+        {title: 'Green', duration: 103.96, audioUrl: 'assets/music/bloc_jams_music/green'},
+        {title: 'Red', duration: 268.45, audioUrl: 'assets/music/bloc_jams_music/red'},
+        {title: 'Pink', duration: 153.14, audioUrl: 'assets/music/bloc_jams_music/pink'},
+        {title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/bloc_jams_music/magenta'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {title: 'Hello, Operator?', duration: '1:01'},
+        {title: 'Ring, ring, ring', duration: '5:01'},
+        {title: 'Fits in your pocket', duration: '3:21'},
+        {title: 'Can you hear me now?', duration: '3:14'},
+        {title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumHHC = {
+    title: 'Summer on Fast Forward',
+    artist: 'Horrible Horrible Creatures',
+    label: 'none',
+    year: '2008',
+    albumArtUrl: 'assets/images/album_covers/HHC cover.png',
+    songs: [
+        {title: 'Every Second Counts', duration: 11, audioUrl: 'assets/music/bloc_jams_music/Every Second Counts'},
+        {title: 'Before This Drive is Over', duration: 187, audioUrl: 'assets/music/bloc_jams_music/Before This Drive is Over'},
+        {title: 'Spiders', duration: 242, audioUrl: 'assets/music/bloc_jams_music/Spiders'},
+        {title: 'My Turn to Win', duration: 267, audioUrl: 'assets/music/bloc_jams_music/My Turn to Win'},
+        {title: 'Hey Self', duration: 136, audioUrl: 'assets/music/bloc_jams_music/Hey Self'},
+        {title: 'Automatic Reset', duration: 215, audioUrl: 'assets/music/bloc_jams_music/Automatic Reset'},
+        {title: 'Lay Quiet Like its Night', duration: 172, audioUrl: 'assets/music/bloc_jams_music/Lay Quiet Like its Night'},
+        ]
+}
