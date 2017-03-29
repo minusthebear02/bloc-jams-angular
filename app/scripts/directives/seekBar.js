6178dc9caa60bba43face8fa991bc1d57e8e919d
Mark Froehlich <mfroeh2@gmail.com>
2017-03-29 00:06:43 -0400

    Angular Assignment 9: Complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -32,6 +32,10 @@
                     return {width: percentString()};
                 };
                 
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
+                };
+                
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
@@ -50,6 +54,8 @@
                         $document.unbind('mouseup.thumb');
                     });
                 };
+                
+                
             }
         };
     }
